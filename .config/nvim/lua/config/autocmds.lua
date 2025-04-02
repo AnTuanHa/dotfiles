03d4a5d305b16b9e34c961ebce5ff364ab38ab40
An Ha <zero579911@gmail.com>
2025-04-02 03:13:05 -0400

    update(neovim): set borders with winborder
    neovim 0.11 deprecated vim.lsp.handlers for hover and signature_help

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,9 +3,5 @@
 --
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-local float = { focusable = true, style = "minimal", border = "rounded", }
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
+
+vim.o.winborder = "rounded"
